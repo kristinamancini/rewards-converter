5b3b5c515aeffc0167311d2bfb4f9f3cb493e008
Kristina Mancini <km36558p@pace.edu>
2023-05-16 15:16:52 -0400

    Add RewardValue class

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,2 +1,22 @@
 public class RewardValue {
+    double cash;
+    int miles;
+
+    RewardValue(double cashValue){
+        cash = cashValue;
+    }
+    RewardValue(int milesValue){
+        miles = milesValue;
+    }
+
+    double getCashValue(){
+        return cash;
+    }
+
+    int getMilesValue(){
+        miles = (int)(cash * (1.0 - .0035));
+        return miles;
+    }
+
+
 }
